8eac07b8a618924086cfe50913e1b2334528db2f
Hemantkumar Panchal <panchalhemant2001@yahoo.com>
2018-05-17 19:49:00 +0000

    <Message> subcomponent integrated in <MessageList> component

--- src/Message.jsx
+++ src/Message.jsx
@@ -2,11 +2,12 @@ import React, {Component} from 'react';
 
 export default class Message extends React.Component {
   render() {
+    const msgId = this.props.id; //for later use
     return (
-        <div className='message'>
-          <span className='message-username'>Anonymous1</span>
-          <span className='message-content'>I won't be impressed with technology until I can download food.</span>
-        </div>
+      <div className='message'>
+          <span className='message-username'>{this.props.username}</span>
+          <span className='message-content'>{this.props.content}</span>
+      </div>
     );
   }
 }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
+import Message from './Message.jsx';
 
 export default class MessageList extends React.Component {
   render() {
       const messages = this.props.messages.map(
-        (message) => <div className='message' key={message.id}>
-            <span className='message-username'>{message.username}</span>
-            <span className='message-content'>{message.content}</span>
-          </div>
+        (message) => <Message
+                        key={message.id}
+                        id={message.id}
+                        username={message.username}
+                        content={message.content}
+                      />
       );
 
       return (
